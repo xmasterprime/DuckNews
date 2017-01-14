512d0b3c7278f1738e452f966d09b6695591f360
DESKTOP-Q6D10N1\dsison <dsison34@hotmail.com>
2017-01-14 12:16:50 -0800

    Added blank settings fragment

--- app/src/main/java/dsison/ducknews/MainActivity.java
+++ app/src/main/java/dsison/ducknews/MainActivity.java
@@ -1,7 +1,11 @@
 package dsison.ducknews;
 
+import android.content.Intent;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/dsison/ducknews/SettingsActivity.java
@@ -0,0 +1,22 @@
+package dsison.ducknews;
+
+/**
+ * Created by dsiso on 1/14/2017.
+ */
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class SearchPreferenceFragment extends PreferenceFragment {
+
+    }
+}
